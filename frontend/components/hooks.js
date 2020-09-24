d159e313c3d0ae70fa8d449956353a2db56ee5b7
Albert Luganga <ahosea15@gmail.com>
2020-09-25 00:40:29 +0300

    added mini dashboard

--- frontend/components/hooks.js
+++ frontend/components/hooks.js
@@ -3,7 +3,6 @@ import useSWR from "swr";
 export const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function useFetchMany(page) {
-  console.log("page", page);
   const { data, error } = useSWR(`http://localhost:3000/reports/sales?page=${page}`, fetcher);
   return {
     data: data,
--- frontend/components/view.js
+++ frontend/components/view.js
@@ -2,6 +2,87 @@ import React, { useState } from "react";
 import { Pagination } from "./pagination";
 import SalesTable from "./table";
 import { useFetchMany } from "./hooks";
+import { chain } from "lodash";
+
+import styles from "./__styles__/view.module.css";
+
+const SaleStatistics = () => {
+  const [fromDate, setFromDate] = useState(undefined);
+  const [toDate, setToDate] = useState(undefined);
+  const [topItems, setTopItems] = useState([]);
+  const [totalProfit, setTotalProfit] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = `http://localhost:3000/reports/salesByDate?fromDate=${fromDate}&toDate=${toDate}`;
+    const result = await fetch(url);
+    const data = await result.json();
+
+    const topItemTypes = chain(data.results)
+      .groupBy("itemType")
+      .map((items, itemType) => ({
+        itemType,
+        count: items.length,
+        totalProfit: items.reduce((a, v) => a + v.total, 0),
+      }))
+      .orderBy("totalProfit", "desc")
+      .value()
+      .slice(0, 5);
+
+    setTopItems(topItemTypes);
+    setTotal(data.total);
+    setTotalProfit(data.results.reduce((acc, sale) => acc + sale.total, 0));
+  };
+
+  return (
+    <section>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label>
+          From Date
+          <input
+            type="date"
+            onChange={(e) => setFromDate(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          To Date
+          <input
+            type="date"
+            onChange={(e) => setToDate(e.target.value)}
+            required
+          />
+        </label>
+        <button className={styles.button} type="submit">
+          Search
+        </button>
+      </form>
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <h3>Total</h3>
+          <p>{total.toLocaleString()}</p>
+        </div>
+        <div className={styles.card}>
+          <h3>Total Profit</h3>
+          <p>{totalProfit.toLocaleString()}</p>
+        </div>
+        <div className={styles.card}>
+          <h3>Top 5 Item Types</h3>
+          {topItems.map((item) => (
+            <>
+              <div className={styles.item}>
+                <span>{item.itemType}</span>
+                <span>{item.totalProfit.toLocaleString()}</span>
+              </div>
+              <hr />
+            </>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default function SalesView() {
   const [page, setPage] = useState(1);
@@ -13,6 +94,12 @@ export default function SalesView() {
 
   return (
     <div>
+      <details>
+        <summary>Open Mini Dashboard</summary>
+        <br />
+        <SaleStatistics />
+      </details>
+      <hr />
       <h1>{data.total.toLocaleString()} sales</h1>
       <SalesTable results={data.results} />
       <Pagination
